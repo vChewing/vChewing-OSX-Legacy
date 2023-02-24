b0cd6648ee1bcf70b2f157351253272d9b083564
ShikiSuen <shikisuen@outlook.com>
2023-02-24 13:59:31 +0800

    InputHandler // Implement Alt+BkSp features.

--- vChewing/Modules/InputHandler_HandleStates.swift
+++ vChewing/Modules/InputHandler_HandleStates.swift
@@ -399,12 +399,21 @@ extension InputHandler {
 
     if isCodePointInputMode {
       if !strCodePointBuffer.isEmpty {
-        strCodePointBuffer = strCodePointBuffer.dropLast(1).description
-        if !strCodePointBuffer.isEmpty {
+        func refreshState() {
           var updatedState = generateStateOfInputting()
           updatedState.tooltipDuration = 0
           updatedState.tooltip = tooltipCodePointInputMode
           delegate.switchState(updatedState)
+        }
+        strCodePointBuffer = strCodePointBuffer.dropLast(1).description
+        if input.modifierFlags == .option {
+          strCodePointBuffer.removeAll()
+          refreshState()
+          isCodePointInputMode = true
+          return true
+        }
+        if !strCodePointBuffer.isEmpty {
+          refreshState()
           return true
         }
       }
@@ -458,7 +467,9 @@ extension InputHandler {
       }
       walk()
     } else {
-      letComposerAndCalligrapherDoBackSpace()
+      _ = input.modifierFlags == .option
+        ? clearComposerAndCalligrapher()
+        : letComposerAndCalligrapherDoBackSpace()
     }
 
     switch isConsideredEmptyForNow {
