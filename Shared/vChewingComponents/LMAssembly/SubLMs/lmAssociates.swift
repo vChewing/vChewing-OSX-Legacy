468801e8b20a34ac6f2a064b84e41db8f9c7391a
ShikiSuen <shikisuen@outlook.com>
2023-03-10 12:59:21 +0800

    Repo // Sync changes from Megrez v2.6.0 update.

--- Shared/vChewingComponents/LMAssembly/SubLMs/lmAssociates.swift
+++ Shared/vChewingComponents/LMAssembly/SubLMs/lmAssociates.swift
@@ -93,7 +93,7 @@ public extension vChewingLM {
       }
     }
 
-    public func valuesFor(pair: Megrez.Compositor.KeyValuePaired) -> [String] {
+    public func valuesFor(pair: Megrez.KeyValuePaired) -> [String] {
       var pairs: [String] = []
       if let arrRangeRecords: [(Range<String.Index>, Int)] = rangeMap[pair.toNGramKey] {
         for (netaRange, index) in arrRangeRecords {
@@ -113,7 +113,7 @@ public extension vChewingLM {
       return pairs.filter { set.insert($0).inserted }
     }
 
-    public func hasValuesFor(pair: Megrez.Compositor.KeyValuePaired) -> Bool {
+    public func hasValuesFor(pair: Megrez.KeyValuePaired) -> Bool {
       if rangeMap[pair.toNGramKey] != nil { return true }
       return rangeMap[pair.value] != nil
     }
