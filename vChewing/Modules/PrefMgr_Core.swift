9cb9b78da480510b83b44630b1007211759093fa
ShikiSuen <shikisuen@outlook.com>
2024-02-11 00:08:55 +0800

    UserDef // + dodgeInvalidEdgeCandidateCursorPosition.

--- vChewing/Modules/PrefMgr_Core.swift
+++ vChewing/Modules/PrefMgr_Core.swift
@@ -104,6 +104,9 @@ import AppKit
   @AppProperty(key: UserDef.kMoveCursorAfterSelectingCandidate.rawValue, defaultValue: true)
   public dynamic var moveCursorAfterSelectingCandidate: Bool
 
+  @AppProperty(key: UserDef.kDodgeInvalidEdgeCandidateCursorPosition.rawValue, defaultValue: true)
+  public dynamic var dodgeInvalidEdgeCandidateCursorPosition: Bool
+
   @AppProperty(key: UserDef.kUseDynamicCandidateWindowOrigin.rawValue, defaultValue: true)
   public dynamic var useDynamicCandidateWindowOrigin: Bool
 
