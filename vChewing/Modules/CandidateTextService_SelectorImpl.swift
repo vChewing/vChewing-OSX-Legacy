c6aa4a304a70fe4465ff27a1310fbd1dd9b522d7
ShikiSuen <shikisuen@outlook.com>
2024-03-20 00:04:52 +0800

    Tekkon // Update to v1.6.0 release.

--- vChewing/Modules/CandidateTextService_SelectorImpl.swift
+++ vChewing/Modules/CandidateTextService_SelectorImpl.swift
@@ -150,7 +150,7 @@ private extension CandidateTextService.Coordinator {
   func prepareTextBookZhuyinReadings(_ param: CandidateTextService.CandidatePairServicable) {
     let newReadings = param.reading.map { currentReading in
       if currentReading.contains("_") { return "_??" }
-      return Tekkon.cnvPhonaToTextbookReading(target: currentReading)
+      return Tekkon.cnvPhonaToTextbookStyle(target: currentReading)
     }
     param.reading = newReadings
   }
