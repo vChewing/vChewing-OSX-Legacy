a4bb9ebc4caee3e281527b055c018cf63193cdde
ShikiSuen <shikisuen@outlook.com>
2025-10-10 19:56:09 +0800

    Repo // Turn AppProperty into class; Turn PrefMgr into struct.

--- vChewing/Modules/PrefMgr_Singleton.swift
+++ vChewing/Modules/PrefMgr_Singleton.swift
@@ -7,14 +7,8 @@
 // requirements defined in MIT License.
 
 extension PrefMgr {
-  public static let shared: PrefMgr = {
-    let result = PrefMgr()
-    result.assignDidSetActions()
-    return result
-  }()
-
-  private func assignDidSetActions() {
-    didAskForSyncingLMPrefs = {
+  public static let shared: PrefMgr = .init(
+    didAskForSyncingLMPrefs: {
       if PrefMgr.shared.phraseReplacementEnabled {
         LMMgr.loadUserPhraseReplacement()
       }
@@ -22,15 +16,13 @@ extension PrefMgr {
         LMMgr.loadUserAssociatesData()
       }
       LMMgr.syncLMPrefs()
-    }
-    didAskForRefreshingSpeechSputnik = {
-      SpeechSputnik.shared.refreshStatus()
-    }
-    didAskForSyncingShiftKeyDetectorPrefs = {
+    },
+    didAskForRefreshingSpeechSputnik: SpeechSputnik.shared.refreshStatus,
+    didAskForSyncingShiftKeyDetectorPrefs: {
       InputSession.theShiftKeyDetector.toggleWithLShift = PrefMgr.shared
         .togglingAlphanumericalModeWithLShift
       InputSession.theShiftKeyDetector.toggleWithRShift = PrefMgr.shared
         .togglingAlphanumericalModeWithRShift
     }
-  }
+  )
 }
