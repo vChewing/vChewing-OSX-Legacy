1c7ab8b854f94b7c25cdb40851d88a86c521590a
ShikiSuen <shikisuen@outlook.com>
2025-10-12 15:13:50 +0800

    SPM // Upgrade Tekkon to v1.7.0.

--- Shared/vChewingComponents/Megrez/9_SwiftImpl_Internals.swift
+++ Shared/vChewingComponents/Megrez/9_SwiftImpl_Internals.swift
@@ -2,6 +2,36 @@
 // ====================
 // This code is released under the SPDX-License-Identifier: `LGPL-3.0-or-later`.
 
+// This package is trying to deprecate its dependency of Foundation, hence this file.
+
+extension StringProtocol {
+  func sliced(by separator: any StringProtocol = "") -> [String] {
+    let selfArray = Array(unicodeScalars)
+    let arrSeparator = Array(separator.description.unicodeScalars)
+    var result: [String] = []
+    var buffer: [Unicode.Scalar] = []
+    var sleepCount = 0
+    for index in selfArray.indices {
+      let currentChar = selfArray[index]
+      let range = index ..< (Swift.min(index + arrSeparator.count, selfArray.count))
+      let ripped = Array(selfArray[range])
+      if ripped.isEmpty { continue }
+      if ripped == arrSeparator {
+        sleepCount = range.count
+        result.append(buffer.map { String($0) }.joined())
+        buffer.removeAll()
+      }
+      if sleepCount < 1 {
+        buffer.append(currentChar)
+      }
+      sleepCount -= 1
+    }
+    result.append(buffer.map { String($0) }.joined())
+    buffer.removeAll()
+    return result
+  }
+}
+
 // MARK: - FIUUID
 
 /// A simple UUID v4 implementation without Foundation.
