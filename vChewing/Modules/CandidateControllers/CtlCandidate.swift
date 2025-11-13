acd7021bb716cc03e75fe52299551797e1db4976
ShikiSuen <shikisuen@outlook.com>
2025-11-13 22:16:47 +0800

    MainAssembly, etc. // Simplifying protocols.

--- vChewing/Modules/CandidateControllers/CtlCandidate.swift
+++ vChewing/Modules/CandidateControllers/CtlCandidate.swift
@@ -29,6 +29,7 @@ open class CtlCandidate: NSWindowController, CtlCandidateProtocol {
   open var currentLayout: UILayoutOrientation = .horizontal
   open var locale: String = ""
   open var useLangIdentifier: Bool = false
+  open var maxLinesPerPage: Int = 1
   open var reverseLookupResult: [String] = []
 
   open var highlightedIndex: Int = .max
@@ -36,7 +37,7 @@ open class CtlCandidate: NSWindowController, CtlCandidateProtocol {
     ofSize: min(196, max(12, Double(UserDefaults.current.integer(forKey: "CandidateListTextSize"))))
   )
 
-  open var delegate: CtlCandidateDelegateCore? {
+  open var delegate: CtlCandidateDelegate? {
     didSet {
       guard let delegate = delegate else { return }
       if delegate.isCandidateState { reloadData() }
@@ -90,6 +91,8 @@ open class CtlCandidate: NSWindowController, CtlCandidateProtocol {
     let blendingAgainstTarget: NSColor = NSApplication.isDarkMode ? NSColor.black : NSColor.white
     return result.blended(withFraction: colorBlendAmount, of: blendingAgainstTarget)!
   }
+
+  open func assignCandidateFont(name: String?, size: Double) {}
 
   @discardableResult
   open func showNextLine() -> Bool {
