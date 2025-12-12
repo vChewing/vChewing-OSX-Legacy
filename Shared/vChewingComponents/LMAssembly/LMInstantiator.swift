48e973ed2cd27f9c88d7bc457a823c7f6dfc7b94
ShikiSuen <shikisuen@outlook.com>
2025-12-13 00:06:22 +0800

    LMInstantiator // Bind `userPhrasesDatabaseBypassed`.

--- Shared/vChewingComponents/LMAssembly/LMInstantiator.swift
+++ Shared/vChewingComponents/LMAssembly/LMInstantiator.swift
@@ -53,6 +53,7 @@ extension LMAssembly {
       public var filterNonCNSReadings = false
       public var deltaOfCalendarYears: Int = -2_000
       public var allowRescoringSingleKanjiCandidates = false
+      public var bypassUserPhrasesData = false
     }
 
     public static var asyncLoadingUserData: Bool = true
@@ -128,6 +129,7 @@ extension LMAssembly {
       config.deltaOfCalendarYears = prefs.deltaOfCalendarYears
       config.allowRescoringSingleKanjiCandidates = prefs.allowRescoringSingleKanjiCandidates
       config.alwaysSupplyETenDOSUnigrams = prefs.enforceETenDOSCandidateSequence
+      config.bypassUserPhrasesData = prefs.userPhrasesDatabaseBypassed
     }
 
     public func resetFactoryJSONModels() {}
