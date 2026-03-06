89ed9c87d5c5da67915f216fd4a2a5789d35ef0b
ShikiSuen <shikisuen@outlook.com>
2026-03-06 17:13:50 +0800

    MainAssembly // Allow importing / exporting user settings.

--- Shared/Extensions/AppKitImpl_NSView.swift
+++ Shared/Extensions/AppKitImpl_NSView.swift
@@ -25,9 +25,11 @@
     }
   }
 
-  // MARK: - NSOpenPanel
+  // MARK: - NSSavePanel
 
-  extension NSOpenPanel {
+  extension NSSavePanel {
+    /// NSSavePanel is the superclass of NSOpenPanel, hence that
+    /// this extension also applies to NSOpenPanel.
     public func beginSheetModal(
       at window: NSWindow?,
       completionHandler handler: @escaping (NSApplication.ModalResponse) -> ()
