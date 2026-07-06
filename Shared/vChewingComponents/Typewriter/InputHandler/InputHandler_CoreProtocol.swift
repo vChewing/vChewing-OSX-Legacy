d07c3f961ffbca162c6e617ae6b7e31ff5e037a3
ShikiSuen <shikisuen@outlook.com>
2026-07-06 21:49:58 +0800

    KBEvent & Typewriter // Fix typos among API names.

--- Shared/vChewingComponents/Typewriter/InputHandler/InputHandler_CoreProtocol.swift
+++ Shared/vChewingComponents/Typewriter/InputHandler/InputHandler_CoreProtocol.swift
@@ -863,7 +863,7 @@ extension InputHandlerProtocol {
        input.commonKeyModifierFlags == [.option, .shift] {
       return "_shift_alt_punctuation_"
     }
-    switch (input.isControlHold, input.isOptionHold) {
+    switch (input.isControlHeld, input.isOptionHeld) {
     case (true, true): return "_alt_ctrl_punctuation_"
     case (true, false): return "_ctrl_punctuation_"
     case (false, true): return "_alt_punctuation_"
