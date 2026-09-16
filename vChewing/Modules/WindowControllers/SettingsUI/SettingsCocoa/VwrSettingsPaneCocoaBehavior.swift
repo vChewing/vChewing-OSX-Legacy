8ed18d04bdaf305e10e4476ed12b065dd657f72d
ShikiSuen <shikisuen@outlook.com>
2026-09-16 21:52:58 +0800

    InputHandler // Add Shift+Space width preference for the empty state.
    (Phase 222 - Shift+Space width preference in empty state)

--- vChewing/Modules/WindowControllers/SettingsUI/SettingsCocoa/VwrSettingsPaneCocoaBehavior.swift
+++ vChewing/Modules/WindowControllers/SettingsUI/SettingsCocoa/VwrSettingsPaneCocoaBehavior.swift
@@ -95,6 +95,10 @@ extension SettingsPanesCocoa {
           )
         }?.boxed()
         NSStackView.buildSection(width: contentWidth) {
+          UserDef.kSpecifyShiftSpaceKeyBehavior4EmptyState.renderCocoa(
+            fixWidth: contentWidth,
+            prefUITab: .tabBehavior
+          )
           UserDef.kSpecifyShiftSpaceKeyBehavior4CandidateWindow.renderCocoa(
             fixWidth: contentWidth,
             prefUITab: .tabBehavior
